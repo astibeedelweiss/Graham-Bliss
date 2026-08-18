b6aaf88b8cd97d97ea2f36dbc3047873f72a2b29
astibeedelweiss <astibeedelweiss_act@plmun.edu.ph>
2026-08-19 00:24:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function orderNow() {
   alert("Thank you for ordering (｡•ᴗ•｡)♡ Please message 0918 529 1039");
 }
+
+let cartItems = 0;
+
+function addToCart(flavor) {
+  cartItems++;
+
+  document.getElementById("cart-count").innerText =
+    "🛒 Items: " + cartItems;
+
+  alert(flavor + " added to cart (｡•ᴗ-)✧");
+}
