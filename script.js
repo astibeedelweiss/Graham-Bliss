6a4d2e512c29ca3ee0fb00a018011d426463016c
astibeedelweiss <astibeedelweiss_act@plmun.edu.ph>
2026-08-19 00:34:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 function orderNow() {
-  alert("Thank you for ordering (｡•ᴗ•｡)♡ Please message 0918 529 1039");
+  alert("Thank you for ordering! (˶ˆᗜˆ˵)♡ Please message 0918 529 1039");
 }
 
-let cartItems = 0;
+let cartItems = [];
+let cartCount = 0;
 
 function addToCart(flavor) {
-  cartItems++;
+  cartItems.push(flavor);
+  cartCount++;
 
-  document.getElementById("cart-count").innerText =
-    "🛒 Items: " + cartItems;
+  updateCartUI();
 
-  alert(flavor + " added to cart (｡•ᴗ-)✧");
+  alert(flavor + " ˚.⋆꒰১ added to cart ໒꒱⋆.˚");
+}
+
+function updateCartUI() {
+  document.getElementById("cart-count").innerText = cartCount;
+
+  let list = document.getElementById("cart-items");
+  list.innerHTML = "";
+
+  cartItems.forEach((item, index) => {
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+      ${item}
+      <button onclick="removeFromCart(${index})">❌</button>
+    `;
+
+    list.appendChild(li);
+  });
+
+  document.getElementById("cart-total").innerText =
+    "Total Items: " + cartCount;
+}
+
+function removeFromCart(index) {
+  cartItems.splice(index, 1);
+  cartCount--;
+
+  updateCartUI();
+}
+
+function toggleCart() {
+  let panel = document.getElementById("cart-panel");
+
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
+  }
 }
